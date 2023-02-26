9650c72f228bbd4c75a307b11ed22d83e387305d
HumbleFool830 <sunilkatta830@gmail.com>
2023-02-26 19:55:36 +0530

    added explanation in comment format

--- sliding_Window/subArraySum.java
+++ sliding_Window/subArraySum.java
@@ -23,7 +23,14 @@ public class subArraySum {
         int sum = 0;
         for(int en=0;en<arr.length;en++)
         {
-            sum += arr[en];
+            sum += arr[en];   
+            /* 
+        first "K" element's of the array will be added to sum because the condition in
+        while statement is in false until end reaches the "k"th index in the array. after that while 
+        condition is true then we are updating the list and removing the leftmost element form sum and incrementing 
+        the start value and iterating over the array until reaches the end element in the array 
+        st --> start , en -->end , sum -->current sub array sum of k elements in array . 
+             */
             while(en-st == k-1)
             {
                 res.add(sum);
